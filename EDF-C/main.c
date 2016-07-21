00ebf24fcf8b2f93fb6b7e5a7504ca0f4497d8b3
rdtek <mrrobindunn@gmail.com>
2016-07-21 14:25:42 +0800

    Implemented ReadInt function and setting header fields.

--- EDF-C/main.c
+++ EDF-C/main.c
@@ -5,18 +5,24 @@
 
 #include "logging.h"
 
-void ReadAscii(FILE * pFile, size_t length, char * charBuff);
+void ReadAscii(FILE * pFile, size_t length, char * charBuffOut);
+void ReadInt(FILE * pFile, size_t length, int * intOut);
 
 int main(void){
 
 	printf("Reading EDF file...");
 	
-	char version[9] = {0};
-	char patientID[81] = {0};
-	char recordID[81] = {0};
-	char startDate[9] = {0};
-	char startTime[9] = {0};
-	
+	char version[9]		= { 0 };
+	char patientID[81]	= { 0 };
+	char recordID[81]	= { 0 };
+	char startDate[9]	= { 0 };
+	char startTime[9]	= { 0 };
+	int numberOfBytesInHeader;
+	char reserved[45]	= { 0 };
+	int numberOfDataRecords;
+	int durationOfDataRecord;
+	int numberOfSignals;
+
 	FILE *pFile = fopen("C:\\temp\\ecg\\snap64.edf", "r");
     if (pFile == NULL) {
 		printf("\nError opening file.");
@@ -28,18 +34,24 @@ int main(void){
 	ReadAscii(pFile, 80,  patientID);	//PatientID
 	ReadAscii(pFile, 80,  recordID); 	//RecordID
 	ReadAscii(pFile, 8,   startDate); 	//StartDate
-	ReadAscii(pFile, 8,   startDate); 	//StartTime
-	//ReadInt16(8); 	//NumberOfBytesInHeader
-	//ReadAscii(44); 	//Reserved
-	//ReadInt16(8); 	//NumberOfDataRecords
-	//ReadInt16(8); 	//DurationOfDataRecord
-	//ReadInt16(4); 	//NumberOfSignals
+	ReadAscii(pFile, 8,   startTime); 	//StartTime
+	ReadInt(pFile, 8, &numberOfBytesInHeader); 	//NumberOfBytesInHeader
+	ReadAscii(pFile, 44, reserved); 			//Reserved
+	ReadInt(pFile, 8, &numberOfDataRecords); 	//NumberOfDataRecords
+	ReadInt(pFile, 8, &durationOfDataRecord); 	//DurationOfDataRecord
+	ReadInt(pFile, 4, &numberOfSignals); 		//NumberOfSignals
 }
 
-void ReadAscii(FILE * pFile, size_t length, char * charBuff){
+void ReadAscii(FILE * pFile, size_t length, char * charBuffOut){
 	int readResult = 0;
-	//unsigned char buf[100] = {0};
-	readResult = fread (&charBuff, 1, length, pFile);
+	readResult = fread (charBuffOut, 1, length, pFile);
 	printf("\n# bytes: %d \t", readResult);
-	printf("%.*s", length, charBuff);
+	printf("%.*s", length, charBuffOut);
 }
+
+void ReadInt(FILE * pFile, size_t length, int * intOut) {
+	char strNumber[10] = {0};
+	ReadAscii(pFile, length, strNumber);
+	*intOut = (int)strtol(strNumber, NULL, 10);
+}
+
